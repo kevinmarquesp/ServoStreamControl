76ff2bd0ce484269556a2e541b5fc0a29ba230ed
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-11-15 11:11:31 -0300

    :alembic: Confirming the behavior of local libraries that I expect

--- ServoStreamControl/ServoStreamControl.ino
+++ ServoStreamControl/ServoStreamControl.ino
@@ -1,21 +1,11 @@
-#include <ParallelServo.h>
-#include <StringSplitter.h>
+#include "src/user/include/SayHello.h"
 
 void setup(void)
 {
-    String rawCommand = "10:20,20:20,30:20,40:20,50:20,60:20";
-
-    StringSplitter* command = new StringSplitter(rawCommand, ',', 6);
-    uint8_t ic = command->getItemCount();
-
     Serial.begin(9600);
-
-    for (int i = 0; i < ic; ++i)
-    {
-        Serial.println(command->getItemAtIndex(i));
-    }
 }
 
 void loop(void)
 {
+    sayHello();
 }
---
+++ ServoStreamControl/src/user/source/SayHello.cpp
@@ -0,0 +1,6 @@
+#include "../include/SayHello.h"
+
+void sayHello(void)
+{
+    Serial.println("Hello world from the src/ library!");
+}
