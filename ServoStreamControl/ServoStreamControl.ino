6e1689ca1755bdf1cac6c87257649343afe88c00
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-11-19 16:07:16 -0300

    :poop: Created the skeleton of what the main ino file will handle the commands

--- ServoStreamControl/ServoStreamControl.ino
+++ ServoStreamControl/ServoStreamControl.ino
@@ -1,20 +1,40 @@
 #include <ParallelServo.h>
+#include "src/stringUtilities.h"
+#include "src/commandFactory.h"
+#include "src/shell/BaseCommand.h"
+#include "src/shell/CmdOutput_t.h"
 
 String readUserStringFromSerialInput(void);
+void logWhenStatusError(const String msg);
+void logWhenStatusOk(const String msg);
 
 void setup(void)
 {
     Serial.begin(9600);
+    delay(1500);
 }
 
 void loop(void)
 {
-    if (!Serial.available())
-        return;
-
     String userInput = readUserStringFromSerialInput();
-    Serial.println(userInput);
 
-    // todo: make the command pallet verification
-    // todo: the first command shoud be the one to fill up the MainServosData variable
+    if (!isThisStringAValidCommandString(userInput))
+    {
+        userInput.concat(" is not an available command string");
+        logWhenStatusError(userInput);
+        return;
+    }
+
+    String cmd, arg;
+    userInput = userInput.substring(1);  // remove the '$' character at the begining of the string
+
+    spliCommandtStringIntoTwoArguments(userInput, cmd, arg);
+
+    BaseCommand* command = commandFactory(cmd, arg);
+    CmdOutput_t commandOutput = command->exec();
+
+    if (commandOutput.isStatusOk)
+        logWhenStatusOk(commandOutput.info);
+    else
+        logWhenStatusError(commandOutput.info);
 }
