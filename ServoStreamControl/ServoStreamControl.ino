3947a3a1643375d03cdc95795ddb201a87652205
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-15 20:10:01 -0300

    :rewind: Deleted the original code of the SSC, but commented the main logic

--- ServoStreamControl/ServoStreamControl.ino
+++ ServoStreamControl/ServoStreamControl.ino
@@ -1,7 +1,39 @@
+#include <LinkedList.h>
+#include <ParallelServo.h>
+/* #include "src/strHelpers.h"
+#include "src/CmdStatus_t.h" */
+
+LinkedList<ParallelServo> Servos;
+
+/* CmdStatus_t cmdStatus; */
+String userSerialInput;
+String cmdPrefix, cmdSufix;
+
+/* String readSerialInputString(void);
+CmdStatus_t choseAndExecute(const String cmdPrefix, const String cmdSufix); */
+
 void setup(void) {
     Serial.begin(9600);
     delay(1500);
 }
 
 void loop(void) {
+    /* userSerialInput = readSerialInputString();
+
+    if (!isCommand(userSerialInput)) {
+        String errorMessage = userSerialInput + String(" is not a command...");
+        printError(errorMessage);
+
+        return;
+    }
+
+    userSerialInput = userSerialInput.substring(1);  //remove the '$' char
+
+    divideString(userSerialInput, cmdPrefix, cmdSufix);
+    cmdStatus = choseAndExecute(cmdPrefix, cmdSufix);
+
+    if (cmdStatus.isOk)
+        printSuccess(cmdStatus.brief);
+    else
+        printError(cmdStatus.brief); */
 }
