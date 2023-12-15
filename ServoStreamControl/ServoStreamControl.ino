6e90fe20977e7c7b448835f4f4c6b829edd3d1ef
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-15 18:45:12 -0300

    :fire: (reset) Removed all files because the factory code is driving me crazy at this rate
    A verdade é que lidar com ponteiros e com factory pattnern do jeito que eu tava fazendo, só tornava o código mais complexo e difícil de entender. Essa é a razão
    desse reset absurdo...

--- ServoStreamControl/ServoStreamControl.ino
+++ ServoStreamControl/ServoStreamControl.ino
@@ -1,45 +1,7 @@
-#include "src/Servos_t.h"
-#include "src/stringUtilities.h"
-#include "src/getCommandFromFactory.h"
-#include "src/shell/BaseCommand.h"
-#include "src/shell/CmdOutput_t.h"
-
-Servos_t Servos;
-
-// As funções abaixo não estão em módulos separados, são arquivos .ino que
-// também estão localizados na raíz desse sketch (do lado desse arquivo aqui)
-String readUserStringFromSerialInput(void);
-void logWhenStatusError(const String msg);
-void logWhenStatusOk(const String msg);
-
-void setup(void)
-{
+void setup(void) {
     Serial.begin(9600);
     delay(1500);
-
-    Servos.size = 0;
 }
 
-void loop(void)
-{
-    String userInputString = readUserStringFromSerialInput();
-
-    if (!isThisStringAValidCommandString(userInputString))
-    {
-        logWhenStatusError(userInputString + String(" is not an available command string"));
-        return;
-    }
-
-    userInputString = userInputString.substring(1);  // remove the '$' character at the begining of the string
-
-    String commandPrefix, commandArguments;
-    splitCommandtStringIntoTwoArguments(userInputString, commandPrefix, commandArguments);
-
-    BaseCommand* command = getCommandFromFactory(commandPrefix, commandArguments);
-    CmdOutput_t commandOutput = command->exec();
-
-    if (commandOutput.isStatusOk)
-        logWhenStatusOk(commandOutput.info);
-    else
-        logWhenStatusError(commandOutput.info);
+void loop(void) {
 }
--- ServoStreamControl/src/shell/CmdOutput_t.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-typedef struct CmdOutput_t
-{
-    const bool isStatusOk;
-    const String info;
-} CmdOutput_t;
