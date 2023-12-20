fd381dd1fbbf63422f0df8105acb48db384df0ac
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-20 16:04:45 -0300

    :construction: Created the command that validates if the user string is a command or not

---
+++ ServoStreamControl/src/stringUtilities.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Arduino.h>
+#include "status_t.h"
+
+inline void cleanUpUserInputString_l(const String* userString) {
+    userString.replace(" ", "");
+}
+
+inline status_t validateCommandString(const String userCommandString) {
+    cleanUpUserInputString_l(&userCommandString);
+
+    if (userCommandString.charAt(0) != '$')
+        return {false, "string specified is not a valid command string"};
+
+    else if (userCommandString.charAt(userCommandString.length()) == ':')
+        return {false, "command prefix (arguments) not specified"};
+
+    return {true, ""};
+}
