dce694017a722074b5727f300b066f3c4bcf8143
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-25 17:12:29 -0300

    :sparkles: Now the function that reads the user input also format it, removing stuff

--- ServoStreamControl/userInteraction.ino
+++ ServoStreamControl/userInteraction.ino
@@ -1,12 +1,16 @@
+#include "src/string.h"
+
 /**
  * Essa função para a execução do programa e espera o usuário enviar alguma
  * coisa na porta serial.
  * @returns String: O que o usuário escreveu na comunicação serial
  */
 inline String readSerialInputString(void) {
-    while (!Serial.available())
-        continue;
+  while (!Serial.available())
+    continue;
 
-    const String userInputString = Serial.readStringUntil('\n');
-    return userInputString;
+  const String userInputString = Serial.readStringUntil('\n');
+  string::cleanInputString(userInputString);
+
+  return userInputString;
 }
