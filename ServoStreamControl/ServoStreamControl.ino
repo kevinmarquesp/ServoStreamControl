9ddbc8d6d22b7c319262bd412839b36b528f43a9
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-25 17:18:00 -0300

    :sparkles: Now the code reads the user input and ignores if its empty

--- ServoStreamControl/ServoStreamControl.ino
+++ ServoStreamControl/ServoStreamControl.ino
@@ -1,30 +1,26 @@
 #include <LinkedList.h>
 #include <ParallelServo.h>
 
-#include "src/stringUtilities.h"
-#include "src/status_t.h"
-
 String readSerialInputString(void);
-void showInput(const String)
+void showInput(const char*);
+void showInput(const String);
 void showError(const char*);
+void showError(const String);
 
 LinkedList<ParallelServo> Servos;
 
 String userSerialInput;
 
 void setup(void) {
-    Serial.begin(9600);
-    delay(1500);
+  Serial.begin(9600);
+  delay(1500);
 }
 
 void loop(void) {
-    userSerialInput = readSerialInputString();
-    showInput(userSerialInput);
+  userSerialInput = readSerialInputString();
 
-    status_t commandStringValidation = validateCommandString(userSerialInput);
-
-    if (not commandStringValidation.isOk) {
-        showError(commandStringValidation.info);
-        return;
-    }
+  // ignores if the string is empty...
+  if (userSerialInput.length() == 0)
+    return;
+  showInput(userSerialInput);
 }
